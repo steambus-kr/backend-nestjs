a67accb03022b67373228ae076f5c1e9fe274858
p-sw <shinwoo.park@psw.kr>
2024-11-29 22:52:32 +0900

    feat: add main health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
 import { LoggedController } from 'nestlogged';
+import { Get } from '@nestjs/common';
 
 @LoggedController()
 export class AppController {
   constructor() {}
+
+  @Get('/health')
+  async health() {
+    return {
+      ok: true,
+    };
+  }
 }
