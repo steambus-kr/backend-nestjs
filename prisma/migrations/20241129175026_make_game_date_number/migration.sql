870feaf577c37777a8bbf2e83fad29a18fe6901d
p-sw <shinwoo.park@psw.kr>
2024-11-30 02:54:40 +0900

    refactor: make Game release_date Date

---
+++ prisma/migrations/20241129175026_make_game_date_number/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `release_date` on the `Game` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `DateTime(3)`.
+
+*/
+
+ALTER TABLE `Game` MODIFY `release_date` VarChar(191) NULL;
+
+UPDATE `Game` SET `release_date`=NULL;
+
+-- AlterTable
+ALTER TABLE `Game` MODIFY `release_date` DATETIME(3) NULL;
