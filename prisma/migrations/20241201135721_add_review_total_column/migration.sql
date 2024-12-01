3b81b1fc9f0276f8f6ce7782471115d3a30d4d3c
p-sw <shinwoo.park@psw.kr>
2024-12-01 23:03:29 +0900

    feat: add review_total column not nullable

--- prisma/migrations/20241201135721_add_review_total_column/migration.sql
+++ prisma/migrations/20241201135721_add_review_total_column/migration.sql
@@ -1,2 +1,8 @@
 -- AlterTable
 ALTER TABLE `Game` ADD COLUMN `review_total` INTEGER NULL;
+
+-- Update
+UPDATE Game SET `review_total` = `review_positive` + `review_negative`;
+
+-- AlterTable
+ALTER TABLE `Game` MODIFY COLUMN `review_total` INTEGER NOT NULL;
